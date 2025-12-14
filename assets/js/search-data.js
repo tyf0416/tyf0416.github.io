6627f8a8b5695ab976b17cc2adb48ba17f0afc6a
tyf0416 <34067211+tyf0416@users.noreply.github.com>
2025-12-14 08:50:18 +0000

    Deploying to gh-pages from @ tyf0416/tyf0416.github.io@385be7138ecebd596570886037da3a361e9742fb 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -3,8 +3,8 @@ const ninja = document.querySelector('ninja-keys');
 
 // add the home and posts menu items
 ninja.data = [{
-    id: "nav-yifan-tang",
-    title: "Yifan Tang",
+    id: "nav-about",
+    title: "about",
     section: "Navigation",
     handler: () => {
       window.location.href = "/";
